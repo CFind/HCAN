e665fe00e9e4f27c90ebedc8b1015436320df603
Connor Findlay <conathan192@gmail.com>
2019-11-25 22:20:16 -0600

    Net Initialization

--- CANTool.Designer.cs
+++ CANTool.Designer.cs
@@ -41,12 +41,16 @@
             this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.rxLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.rxLabel = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.baudrateLabel = new System.Windows.Forms.Label();
+            this.netInitButton = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.rxDataGrid)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -55,7 +59,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(856, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1129, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -70,8 +74,9 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
             this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // timer1
             // 
@@ -80,9 +85,9 @@
             // baudrateComboBox
             // 
             this.baudrateComboBox.FormattingEnabled = true;
-            this.baudrateComboBox.Location = new System.Drawing.Point(12, 356);
+            this.baudrateComboBox.Location = new System.Drawing.Point(12, 568);
             this.baudrateComboBox.Name = "baudrateComboBox";
-            this.baudrateComboBox.Size = new System.Drawing.Size(121, 21);
+            this.baudrateComboBox.Size = new System.Drawing.Size(75, 21);
             this.baudrateComboBox.TabIndex = 9;
             this.baudrateComboBox.SelectedIndexChanged += new System.EventHandler(this.baudrateComboBox_SelectedIndexChanged);
             // 
@@ -95,7 +100,8 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.rxDataGrid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.rxDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.rxDataGrid.ColumnHeadersHeight = 24;
+            this.rxDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.rxDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn7,
             this.dataGridViewTextBoxColumn8,
@@ -103,46 +109,74 @@
             this.dataGridViewTextBoxColumn10,
             this.dataGridViewTextBoxColumn11,
             this.dataGridViewTextBoxColumn12});
-            this.rxDataGrid.Location = new System.Drawing.Point(48, 3);
+            this.rxDataGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.rxDataGrid.Location = new System.Drawing.Point(39, 3);
+            this.rxDataGrid.MultiSelect = false;
             this.rxDataGrid.Name = "rxDataGrid";
-            this.rxDataGrid.Size = new System.Drawing.Size(805, 112);
+            this.rxDataGrid.RowHeadersVisible = false;
+            this.rxDataGrid.Size = new System.Drawing.Size(1087, 258);
             this.rxDataGrid.TabIndex = 8;
             // 
             // dataGridViewTextBoxColumn7
             // 
-            this.dataGridViewTextBoxColumn7.HeaderText = "CAN-ID";
+            this.dataGridViewTextBoxColumn7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn7.HeaderText = "ID";
             this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
             this.dataGridViewTextBoxColumn7.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn8
             // 
+            this.dataGridViewTextBoxColumn8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.dataGridViewTextBoxColumn8.HeaderText = "Type";
             this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
             this.dataGridViewTextBoxColumn8.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn9
             // 
-            this.dataGridViewTextBoxColumn9.HeaderText = "Data Length";
+            this.dataGridViewTextBoxColumn9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn9.HeaderText = "DLC";
             this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
             this.dataGridViewTextBoxColumn9.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn10
             // 
+            this.dataGridViewTextBoxColumn10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.dataGridViewTextBoxColumn10.HeaderText = "Msg Data";
             this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
             this.dataGridViewTextBoxColumn10.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn11
             // 
+            this.dataGridViewTextBoxColumn11.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.dataGridViewTextBoxColumn11.HeaderText = "Cycle Time";
             this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
             this.dataGridViewTextBoxColumn11.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn12
             // 
-            this.dataGridViewTextBoxColumn12.HeaderText = "# Msg Recieved";
+            this.dataGridViewTextBoxColumn12.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn12.HeaderText = "Recieve Count";
             this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
             this.dataGridViewTextBoxColumn12.ReadOnly = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91.52778F));
+            this.tableLayoutPanel1.Controls.Add(this.rxLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rxDataGrid, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.89421F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.10579F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1129, 501);
+            this.tableLayoutPanel1.TabIndex = 7;
             // 
             // rxLabel
             // 
@@ -152,54 +186,57 @@
             this.rxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rxLabel.Location = new System.Drawing.Point(3, 0);
             this.rxLabel.Name = "rxLabel";
-            this.rxLabel.Size = new System.Drawing.Size(39, 118);
-            this.rxLabel.TabIndex = 6;
+            this.rxLabel.Size = new System.Drawing.Size(30, 264);
+            this.rxLabel.TabIndex = 9;
             this.rxLabel.Text = "RX";
             this.rxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // tableLayoutPanel1
+            // splitContainer1
             // 
-            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 45F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91.52778F));
-            this.tableLayoutPanel1.Controls.Add(this.rxLabel, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.rxDataGrid, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.11784F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 54.88216F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(856, 262);
-            this.tableLayoutPanel1.TabIndex = 7;
+            this.splitContainer1.Location = new System.Drawing.Point(39, 267);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(1087, 231);
+            this.splitContainer1.SplitterDistance = 473;
+            this.splitContainer1.TabIndex = 10;
             // 
             // baudrateLabel
             // 
             this.baudrateLabel.AutoSize = true;
-            this.baudrateLabel.Location = new System.Drawing.Point(12, 340);
+            this.baudrateLabel.Location = new System.Drawing.Point(12, 552);
             this.baudrateLabel.Name = "baudrateLabel";
             this.baudrateLabel.Size = new System.Drawing.Size(50, 13);
             this.baudrateLabel.TabIndex = 10;
             this.baudrateLabel.Text = "Baudrate";
             // 
+            // netInitButton
+            // 
+            this.netInitButton.Location = new System.Drawing.Point(103, 566);
+            this.netInitButton.Name = "netInitButton";
+            this.netInitButton.Size = new System.Drawing.Size(75, 23);
+            this.netInitButton.TabIndex = 11;
+            this.netInitButton.Text = "Initialize";
+            this.netInitButton.UseVisualStyleBackColor = true;
+            this.netInitButton.Click += new System.EventHandler(this.netInitButton_Click);
+            // 
             // CANTool
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(856, 415);
+            this.ClientSize = new System.Drawing.Size(1129, 606);
+            this.Controls.Add(this.netInitButton);
             this.Controls.Add(this.baudrateLabel);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.baudrateComboBox);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "CANTool";
-            this.Text = "Form1";
+            this.Text = "CANTool";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.rxDataGrid)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,15 +249,17 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ComboBox baudrateComboBox;
         private System.Windows.Forms.DataGridView rxDataGrid;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label baudrateLabel;
+        private System.Windows.Forms.Label rxLabel;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn12;
-        private System.Windows.Forms.Label rxLabel;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label baudrateLabel;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button netInitButton;
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -18,7 +18,6 @@ namespace HCAN
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CANTool());
-            
         }
     }
 }
